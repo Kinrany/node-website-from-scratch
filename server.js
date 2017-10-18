d83d22f166b8576b16976ece34a61516e18514d3
Kinrany <kinrany@yandex.com>
2017-10-18 19:19:55 +0300

    added koa-router

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-})
+const Router = require('koa-router');
+const router = new Router();
+
+router.get('/', (ctx) => {
+    ctx.status = 200;
+    ctx.body = 'Hello world';
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000);
